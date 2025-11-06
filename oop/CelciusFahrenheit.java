743a4fc1c10bc9aac7f4fd49d1719b4d665e6511
IllalRajinCoding <roxyuciha@gmail.com>
2025-11-06 14:53:44 +0700

    push

--- oop/CelciusFahrenheit.java
+++ oop/CelciusFahrenheit.java
@@ -18,5 +18,11 @@ public class CelciusFahrenheit implements KonverterSatuan {
     public float konversi(float in){
         return 9f / 5 * in + 32;
     }
+
+    public static void main(String[] args){
+        KonverterSatuan k1 = new CelciusFahrenheit();
+        float tmp = 100;
+        System.out.println("Titik didih air " + tmp + " " + k1.satuanInput() + " = " + k1.konversi(tmp) + " " + k1.satuanOutput());
+    }
     
 }
